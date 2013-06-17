9630518c0d9d36d7179a42eb169f1df4173729be
Eric Kryski <e.kryski@gmail.com>
2013-06-17 14:53:34 -0600

    fixed logging typo. Exposing cheerio

--- utils/index.js
+++ utils/index.js
@@ -4,6 +4,7 @@ Utils.request = require('./request');
 Utils.date = require('./date');
 Utils._ = require('underscore');
 Utils.str = Utils._.str = require('./string');
+Utils.$ = require('cheerio');
 
 // Mix in non-conflict functions to Utils._ namespace
 Utils._.mixin(Utils._.str.exports());
