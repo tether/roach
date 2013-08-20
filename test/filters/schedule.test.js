8752c5488be2e810c04ba7e103c3cfa75a309be2
Eric Kryski <e.kryski@gmail.com>
2013-08-20 16:16:18 -0600

    fixed some broken tests for schedule filter

--- test/filters/schedule.test.js
+++ test/filters/schedule.test.js
@@ -10,7 +10,7 @@ describe("roach.test.filters.schedule", function() {
     // SETUP
 
     var params = {
-      date: date('Aug 21, 2013'), //this is a Wednesday
+      date: date('Aug 21, 2013').setZone('MST'), //this is a Wednesday
       schedule: {
         daysOfWeek: ['monday', 'tuesday', 'wednesday']
       }
@@ -26,7 +26,7 @@ describe("roach.test.filters.schedule", function() {
   it("should return [] if today doesn't match one of the weekdays", function() {
     // SETUP
     var params = {
-      date: date('Aug 23, 2013'), //this is a Friday
+      date: date('Aug 23, 2013').setZone('MST'), //this is a Friday
       schedule: {
         daysOfWeek: ['monday', 'tuesday', 'wednesday']
       }
@@ -42,7 +42,7 @@ describe("roach.test.filters.schedule", function() {
   it("should return the document if today matches a day for a weekly repeat rule", function() {
     // SETUP
     var params = {
-      date: date('Aug 21, 2013'), //this is a Wednesday
+      date: date('Aug 21, 2013').setZone('MST'), //this is a Wednesday
       schedule: {
         repeat: {
           duration: 'weeks',
@@ -63,7 +63,7 @@ describe("roach.test.filters.schedule", function() {
   it("should return [] if today matches a day for a weekly repeat rule", function() {
     // SETUP
     var params = {
-      date: date('Aug 22, 2013'), //this is a Thursday
+      date: date('Aug 22, 2013').setZone('MST'), //this is a Thursday
       schedule: {
         repeat: {
           duration: 'weeks',
@@ -84,7 +84,7 @@ describe("roach.test.filters.schedule", function() {
   it("should return the document if the duration matches a duration for a weekly repeat rule", function() {
     // SETUP
     var params = {
-      date: date('Sept 3, 2013'), //this is a Tuesday
+      date: date('Sept 3, 2013').setZone('MST'), //this is a Tuesday
       schedule: {
         repeat: {
           duration: 'weeks',
@@ -105,7 +105,7 @@ describe("roach.test.filters.schedule", function() {
   it("should return [] if the duration doesn't match a duration for a an alternating weekly repeat rule", function() {
     // SETUP
     var params = {
-      date: date('Sept 4, 2013'), //this is a Wednesday
+      date: date('Sept 4, 2013').setZone('MST'), //this is a Wednesday
       schedule: {
         repeat: {
           duration: 'weeks',
