384c1e2aa8c491fb2f0c8325705c35a8c34169a3
bredele <olivier.wietrich@gmail.com>
2014-03-11 16:27:21 -0600

    add jobs

--- test/api.js
+++ test/api.js
@@ -13,9 +13,22 @@ describe("API", function() {
 		});
 	});
 	
-	describe("jobs", function() {
+	describe("job", function() {
+
+		var job;
+		beforeEach(function() {
+			job = roach.bug();
+		});
+		
 		it("should expose a function constructor to build jobs", function() {
 			assert.equal(typeof roach.bug, 'function');	
+		});
+
+		it('should inherit from an emitter', function() {
+			assert.equal(typeof job.on, 'function');
+			assert.equal(typeof job.emit, 'function');
+			assert.equal(typeof job.once, 'function');
+			assert.equal(typeof job.off, 'function');
 		});
 		
 	});
