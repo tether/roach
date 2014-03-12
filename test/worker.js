ed12a6cf56f45fcfeb4848e897752738404e098d
bredele <olivier.wietrich@gmail.com>
2014-03-12 13:16:40 -0600

    worker constructor initialize a redis client and a job

--- test/worker.js
+++ test/worker.js
@@ -14,11 +14,25 @@ describe("Worker", function() {
 		client.quit();
 	});
 
-	it("should create a redis client", function(done) {
+	it("should be a redis client", function(done) {
 		client.hset("hash key", "hashtest 1", "some value");
 		client.hkeys("hash key", function (err, replies) {
 			if(replies[0] === "hashtest 1") done();
 		});
 	});
+
+	it("should be a job", function() {
+		assert.equal(worker.job, job);
+		
+	});
+	
+
+	describe("start", function() {
+		it("should start a job", function() {
+			
+		});
+		
+	});
+	
 	
 });
