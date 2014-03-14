675c98a50203059d660b9d8b1a7c20831ae35c55
bredele <olivier.wietrich@gmail.com>
2014-03-14 14:13:15 -0600

    add job into the queue

--- test/queue.js
+++ test/queue.js
@@ -4,15 +4,30 @@ var Queue = require('../lib/queue'),
 
 var client = redis.createClient();
 
-describe("Initialization", function() {
+describe("Add", function() {
 	var queue;
 	beforeEach(function() {
+		Queue.key = 'test:jobs';
 		queue = new Queue();
 	});
-	
-	it('should initialize a job id key', function(done) {
-		client.exists('roach:jobs:id', function(err, res) {
-			if(res === 1) done();
-		});
+
+	afterEach(function() {
+		client.lpop(Queue.key);
 	});
+	
+	it("should add a job into the queue list", function(done) {
+
+		queue.add('test', function(err, r) {
+			client.lrange(Queue.key, 0, 0, function(err, res) {
+				if(res[0] === 'test') done();
+			});
+		});
+
+	});
+	
+	// it('should increment a job id key', function(done) {
+	// 	client.exists('roach:jobs:id', function(err, res) {
+	// 		if(res === 1) done();
+	// 	});
+	// });
 });
