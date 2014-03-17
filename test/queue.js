8a2c97046c957443f97bbcd929092d6ab44b3cd9
bredele <olivier.wietrich@gmail.com>
2014-03-17 12:35:14 -0600

    work on a local queue

--- test/queue.js
+++ test/queue.js
@@ -1,50 +1,72 @@
 var Queue = require('../lib/queue'),
-		assert = require('assert'),
-		redis = require('redis');
+    assert = require('assert'),
+    redis = require('redis');
 
-var client = redis.createClient();
+
+describe("Local", function() {
+
+  describe("Add", function() {
+
+    var queue;
+    beforeEach(function() {
+      queue = new Queue();
+    });
+
+    it("should queue events", function(done) {
+      queue.add('event');
+      queue.on('added event', function(val) {
+        done();
+      });
+    });
+    
+  });
+  
+});
+
+
+// var client = redis.createClient();
 
 // describe("Add job", function() {
 
-// 	var queue;
-// 	beforeEach(function() {
-// 		Queue.key = 'test:jobs';
-// 		queue = new Queue();
-// 	});
+//  var queue;
+//  beforeEach(function() {
+//    Queue.key = 'test:jobs';
+//    queue = new Queue();
+//  });
 
-// 	afterEach(function() {
-// 		client.lpop(Queue.key);
-// 		client.set(Queue.key + ':id', 0);
-// 	});
-	
-// 	it("should add a job into the queue list", function(done) {
+//  afterEach(function() {
+//    client.lpop(Queue.key);
+//    client.set(Queue.key + ':id', 0);
+//  });
+  
+//  it("should add a job into the queue list", function(done) {
 
-// 		queue.add('test', function(err, r) {
-// 			client.lrange(Queue.key, 0, 0, function(err, res) {
-// 				if(res[0] === 'test') done();
-// 			});
-// 		});
+//    queue.add('test', function(err, r) {
+//      client.lrange(Queue.key, 0, 0, function(err, res) {
+//        if(res[0] === 'test') done();
+//      });
+//    });
 
-// 	});
-	
-// 	it('should increment a job id key', function(done) {
+//  });
+  
+//  it('should increment a job id key', function(done) {
 
-// 		queue.add('test');
-// 		queue.add('other', function() {
-// 			client.get(Queue.key + ':id', function(err, res) {
-// 				if(Number(res) === 2) done();
-// 			});
-// 		});
+//    queue.add('test');
+//    queue.add('other', function() {
+//      client.get(Queue.key + ':id', function(err, res) {
+//        if(Number(res) === 2) done();
+//      });
+//    });
 
-// 	});
+//  });
 
-// 	it("should send notif when job is added", function(done) {
-// 		queue.on('added', function() {
-// 			done();
-// 		});
-// 		queue.add('test');
-// 	});
-	
+//  it("should send notif when job is added", function(done) {
+//    queue.on('added', function() {
+//      done();
+//    });
+//    queue.add('test');
+//  });
+  
 
 
 // });
