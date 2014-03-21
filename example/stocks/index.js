ae5c85673b3ada8695cd0b282ee9350b6c1389d7
bredele <olivier.wietrich@gmail.com>
2014-03-21 15:09:47 -0600

    add job redis client

--- example/stocks/index.js
+++ example/stocks/index.js
@@ -1,7 +1,14 @@
-var roach = require('../..');
+var roach = require('../..'),
+		client = require('redis').createClient();
 
 var job = module.exports = roach.job();
 
 job.on('start', function() {
 	console.log('stocks');
 });
+
+client.on('message', function(channel, message) {
+	console.log(channel, message);
+});
+
+client.subscribe("channel");
