306ddec809e7ce275a7cb1ec84fd3c7870f17092
bredele <olivier.wietrich@gmail.com>
2014-03-26 14:15:57 -0600

    request file or http can be piped

--- test/crawler.js
+++ test/crawler.js
@@ -1,48 +1,60 @@
-var roach = require('..'),
-assert = require('assert');
+var roach = require('..');
+var assert = require('assert');
+var writable = require('stream').Writable;
 
 describe("Mixin", function() {
 
-	it("should mixin object", function() {
-		var crawler = roach.crawler({
-			custom:function() {
+  it("should mixin object", function() {
+    var crawler = roach.crawler({
+      custom:function() {
 
-			}
-		});
-		assert(crawler.custom);
-	});
+      }
+    });
+    assert(crawler.custom);
+  });
 
 });
 
 describe("Get file", function() {
 
-	var crawler;
-	beforeEach(function() {
-		crawler = roach.crawler();
-	});
+  var crawler, ws;
+  beforeEach(function() {
+    crawler = roach.crawler();
+    ws = writable();
+    //this is wrong
+    ws._write = function (chunk, enc, next) {
+      next();
+    };
+  });
 
-	it("should have a http handler", function(done) {
-		crawler.http('http://rawgithub.com/petrofeed/roach/master/README.md', function(err, res, body) {
-			if(!err) done();
-		});
-	});
+  it("should have a http handler", function(done) {
+    crawler.http('http://rawgithub.com/petrofeed/roach/master/README.md', function(err, res, body) {
+      if(!err) done();
+    });
+  });
 
-	it('should have a file handler', function(done) {
-		crawler.file(__dirname + '/crawler.js', function(err) {
-			if(!err) done();
-		});
-	});
-	
-	it("should get from http://", function(done) {
-		crawler.get('http://rawgithub.com/petrofeed/roach/master/README.md', function(err) {
-			if(!err) done();
-		});
-	});
+  it('should have a file handler', function(done) {
+    ws.on('finish', done);
+    crawler.file(__dirname + '/crawler.js').pipe(ws);
+  });
+  
+  it("should get from http://", function(done) {
+    ws.on('finish', done);
+    crawler.get('http://rawgithub.com/petrofeed/roach/master/README.md').pipe(ws);
+  });
 
-	it('should get from file://', function(done) {
-		crawler.get('file://' + __dirname + '/crawler.js', function(err) {
-			if(!err) done();
-		});
-	});
+  it('should get from file://', function(done) {
+    ws.on('finish', done);
+    crawler.get('file://' + __dirname + '/crawler.js').pipe(ws);
+  });
 
+  it('should get from constructor', function(done) {
+    ws.on('finish', done);
+    crawler('file://' + __dirname + '/crawler.js').pipe(ws);
+  });
+
+});
+
+describe("Unzip", function() {
+  
 });
