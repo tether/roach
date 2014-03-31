e3a571a2a71ebbf8356522a33d726f3308e7ffa0
bredele <olivier.wietrich@gmail.com>
2014-03-31 03:26:44 -0600

    update flush

--- utils/flush.js
+++ utils/flush.js
@@ -1,13 +1,7 @@
 var client = require('redis').createClient();
 
-
-client.lrange('roach:jobs', 0, -1, function(err, ids) {
-  for(var l = ids.length; l--;) {
-   client.lrem('roach:jobs', 0, ids[l], function() {
-    //hkeys
-    process.exit();
-   });
-  }
-  client.set('roach:jobs:id', 0);
+client.del('roach:jobs', function(err) {
+	client.set('roach:jobs:id', 0, function() {
+		process.exit();
+	});
 });
-
