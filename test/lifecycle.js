a9b24f1aed9364a1d9d502c0a2a82fa5a3320e9a
bredele <olivier.wietrich@gmail.com>
2014-04-02 14:00:37 -0600

    remove from pending and add in active

--- test/lifecycle.js
+++ test/lifecycle.js
@@ -2,6 +2,12 @@ var roach = require('..');
 var assert = require('assert');
 var client = require('redis').createClient();
 
+function check(db, id, done) {
+	client.zrank(db, id, function(err, res) {
+		if(!err) done();
+	});
+}
+
 //verbose tests
 describe('Lifecycle > ', function() {
 
@@ -17,26 +23,30 @@ describe('Lifecycle > ', function() {
 		var job;
 		beforeEach(function() {
 			job = roach.job();
-			server.add('job', job);
+			server.add('job');
 		});
 
 		describe('pending > ', function() {
 
 			it('is added in the pending queue', function(done) {
-				var check = function(id) {
-					client.zrank('roach:jobs:pending', id, function(err) {
-						if(!err) done();
-					});
-				};
-				queue.on('added job', check);
+				queue.on('added job', function(id) {
+					check('roach:jobs:pending', id, done);
+				});
 			});
 
 		});
 
 		describe('active', function() {
 
-			it('is added in active queue on start', function() {
-				
+			it('is added in active queue and removed from pending on start', function(done) {
+				job.start(function(id) {
+					check('roach:jobs:active', id, function() {
+						client.zrank('roach:jobs:pending', id, function(err, res) {
+							if(!err && !res) done();
+						});
+					});
+				});
+				server.use('job', job);
 			});
 
 			it('send progress and log events', function() {
