1c27d2b633d04eff8a90687b64ee107850f365b1
Eric Kryski <e.kryski@gmail.com>
2014-04-11 16:18:46 -0600

    adding the start of the API. Closes #5. Refs #6

--- example/traffic/index.js
+++ example/traffic/index.js
@@ -4,7 +4,7 @@ var job = module.exports = roach.job();
 //parse file on start
 
 job.start(function() {
-	job.log('start stocks');
+	job.log('start traffic');
 	var i = 10;
 	var interval = setInterval(function() {
 		if(i === 100) clearInterval(interval);
@@ -16,16 +16,16 @@ job.start(function() {
 
 
 //test steps
-job.step(function() {
-	console.log('step1');
-}, function() {
-	console.log('step1 bis');
-}, function() {
-	console.log('step1 bis bis');
-});
+// job.step(function() {
+// 	console.log('step1');
+// }, function() {
+// 	console.log('step1 bis');
+// }, function() {
+// 	console.log('step1 bis bis');
+// });
 
-job.step(function(data) {
-	console.log('step2');
-});
+// job.step(function(data) {
+// 	console.log('step2');
+// });
 
-job.emit('job run');
+// job.emit('job run');
